07c2d69ec21aa89325c460f941a7fc0c841f40a2
Tristan Copley <89219797+TristanCopley@users.noreply.github.com>
2023-03-27 22:02:01 -0700

    Updates

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,36 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  text_icons = {
+    ActiveLSP = "LSP:",
+    ActiveTS = "",
+    BufferClose = "x",
+    DefaultFile = "[F]",
+    Diagnostic = "",
+    DiagnosticError = "X",
+    DiagnosticHint = "?",
+    DiagnosticInfo = "i",
+    DiagnosticWarn = "!",
+    Ellipsis = "...",
+    FileModified = "*",
+    FileReadOnly = "[lock]",
+    FolderClosed = "[D]",
+    FolderEmpty = "[E]",
+    FolderOpen = "[O]",
+    Git = "",
+    GitAdd = "[+]",
+    GitBranch = "",
+    GitChange = "[/]",
+    GitConflict = "[!]",
+    GitDelete = "[-]",
+    GitIgnored = "[I]",
+    GitRenamed = "[R]",
+    GitStaged = "[S]",
+    GitUnstaged = "[U]",
+    GitUntracked = "[?]",
+    LSPLoaded = "",
+    LSPLoading1 = "",
+    LSPLoading2 = "",
+    LSPLoading3 = "",
+    MacroRecording = "Recording:",
+    NeovimClose = "X",
+  },
 }
